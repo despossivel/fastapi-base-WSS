d4da8fa8d49d42c5ecc5009b4351cb08b76bfe5c
Matheus Henrique Sousa Brito <matheus.henriquesousabrito@einstein.br>
2023-09-10 13:38:27 -0300

    feat: imporve with controllers

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from src.jwt_auth import generate_token, verify_token
 from src.config.database import create_database
 from src.models.tasks import create_task, select_tasks
 from werkzeug.security import check_password_hash, generate_password_hash
+from src.controllers.tasks import controller_authenticate, controller_list_tasks, controller_add_task
 from dotenv import load_dotenv
 import os
 
@@ -11,37 +12,17 @@ load_dotenv()
  
 app = Flask(__name__)
  
-users = {
-   'user': generate_password_hash(os.getenv('PASSWORD'))
-}
-
 @app.route('/add_task', methods=['POST'])
 def add_task():
-    data = request.get_json()
-    title = data.get('title')
-    
-    if title: 
-        create_task(title)
-        return jsonify({'message': 'Task added successfully!'}), 201
-    else:
-        return jsonify({'error': 'Title is required!'}), 400
+    return controller_add_task()
 
 @app.route('/list_tasks', methods=['GET'])
 def list_tasks():
-    tasks_ = select_tasks()
-    return jsonify({'tasks': tasks_})
+   return controller_list_tasks()
 
 @app.route('/authenticate', methods=['POST'])
 def authenticate():
-    data = request.get_json()
-    username = data.get('username')
-    password = data.get('password')
-
-    if username in users and check_password_hash(users[username], password):
-        token = generate_token(username)
-        return jsonify({'token': token}), 200
-    else:
-        return jsonify({'error': 'Authentication failed'}), 401
+   return controller_authenticate()
 
 if __name__ == '__main__':
     create_database()
